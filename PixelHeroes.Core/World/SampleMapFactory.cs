e85eadb841d8b48673c63762398d25deb5cb2688
skalogryz <skalogryz.lists@gmail.com>
2026-05-15 22:18:43 -0400

    adding the ability to load homm map

--- PixelHeroes.Core/World/SampleMapFactory.cs
+++ PixelHeroes.Core/World/SampleMapFactory.cs
@@ -4,7 +4,7 @@ namespace PixelHeroes.Core.World
     {
         public static AdventureMap CreateStartingMap()
         {
-            var map = new AdventureMap("Valley of Dawn", 16, 10);
+            var map = new AdventureMap("Valley of Dawn", 32, 20);
 
             for (var y = 0; y < map.Height; y++)
             {
@@ -13,7 +13,11 @@ namespace PixelHeroes.Core.World
                     var terrain = TerrainType.Grass;
                     var passable = true;
 
-                    if (y == 4 && x > 1 && x < 14)
+                    if ((y == 4 || y == 12) && x > 1 && x < map.Width - 2)
+                    {
+                        terrain = TerrainType.Road;
+                    }
+                    else if (x == 18 && y > 4 && y < 13)
                     {
                         terrain = TerrainType.Road;
                     }
@@ -22,16 +26,16 @@ namespace PixelHeroes.Core.World
                         terrain = TerrainType.Mountain;
                         passable = false;
                     }
-                    else if ((x == 4 && y < 4) || (x == 11 && y > 5))
+                    else if ((x == 4 && y < 10) || (x == 11 && y > 5) || (x == 22 && y > 8))
                     {
                         terrain = TerrainType.Forest;
                     }
-                    else if (x > 12 && y < 4)
+                    else if (x > 24 && y < 7)
                     {
                         terrain = TerrainType.Water;
                         passable = false;
                     }
-                    else if (x > 6 && x < 10 && y > 5)
+                    else if ((x > 6 && x < 10 && y > 5) || (x > 17 && x < 25 && y > 13))
                     {
                         terrain = TerrainType.Dirt;
                     }
@@ -44,13 +48,19 @@ namespace PixelHeroes.Core.World
             map.AddLocation(new SpecialLocation("sawmill_01", "Old Sawmill", SpecialLocationType.Sawmill, new MapPosition(5, 2), false));
             map.AddLocation(new SpecialLocation("mine_01", "Ore Pit", SpecialLocationType.Mine, new MapPosition(9, 7), false));
             map.AddLocation(new SpecialLocation("shrine_01", "Shrine of Speed", SpecialLocationType.Shrine, new MapPosition(13, 5), false));
+            map.AddLocation(new SpecialLocation("castle_blue", "Blue Hold", SpecialLocationType.Castle, new MapPosition(28, 12), false));
+            map.AddLocation(new SpecialLocation("dwelling_01", "Wolf Den", SpecialLocationType.Dwelling, new MapPosition(22, 15), false));
 
             map.AddItem(new MapItem("gold_01", "Gold Pile", ResourceType.Gold, 750, new MapPosition(6, 4)));
             map.AddItem(new MapItem("wood_01", "Wood Bundle", ResourceType.Wood, 8, new MapPosition(3, 7)));
             map.AddItem(new MapItem("ore_01", "Ore Cart", ResourceType.Ore, 6, new MapPosition(10, 4)));
+            map.AddItem(new MapItem("gems_01", "Gems", ResourceType.Gems, 4, new MapPosition(20, 12)));
+            map.AddItem(new MapItem("crystal_01", "Crystal", ResourceType.Crystal, 3, new MapPosition(27, 6)));
 
             map.AddUnit(new MapUnit("peasants_01", "Peasants", "Neutral", 24, new MapPosition(8, 3)));
             map.AddUnit(new MapUnit("wolves_01", "Wolves", "Neutral", 12, new MapPosition(12, 7)));
+            map.AddUnit(new MapUnit("goblins_01", "Goblins", "Neutral", 20, new MapPosition(18, 10)));
+            map.AddUnit(new MapUnit("dwarves_01", "Dwarves", "Neutral", 10, new MapPosition(25, 16)));
 
             return map;
         }
--- Scripts/GameMap.cs
+++ Scripts/GameMap.cs
@@ -1,193 +1,347 @@
+using System;
+using System.Collections.Generic;
 using Godot;
 using PixelHeroes.Core.World;
 
 public class GameMap : Control
 {
-    private const int TileSize = 48;
-    private AdventureMap adventureMap;
+	private const int TileSize = 48;
+	private const float MinZoom = 0.25f;
+	private const float MaxZoom = 2.0f;
+	private const float ZoomStep = 0.1f;
+	private const int EdgeScrollSize = 32;
+	private const int EdgeScrollSpeed = 420;
+	private AdventureMap adventureMap;
+	private GridContainer renderedMap;
+	private ScrollContainer mapScroll;
+	private HSlider zoomSlider;
+	private Label zoomLabel;
+	private float zoom = 1.0f;
+	private bool isPanning;
+	private Vector2 lastPanMousePosition;
 
-    public override void _Ready()
-    {
-        GetNode<Button>("Header/TownButton").Connect("pressed", this, nameof(OpenTown));
-        GetNode<Button>("Header/BattleButton").Connect("pressed", this, nameof(OpenBattle));
-        GetNode<Button>("Header/SettingsButton").Connect("pressed", this, nameof(OpenSettings));
-        GetNode<Button>("Header/MenuButton").Connect("pressed", this, nameof(OpenMenu));
+	List<Node> added = new List<Node>();
 
-        adventureMap = GameSession.TakeMapOrDefault();
-        GetNode<Label>("Header/Title").Text = $"Adventure Map - {adventureMap.Name}";
-        RenderAdventureMap();
-    }
+	public override void _Ready()
+	{
+		SetProcess(true);
+		SetProcessInput(true);
 
-    private void RenderAdventureMap()
-    {
-        var mapRoot = GetNode<Control>("MapPlaceholder");
-        foreach (Node child in mapRoot.GetChildren())
-        {
-            child.QueueFree();
-        }
+		GetNode<Button>("Header/TownButton").Connect("pressed", this, nameof(OpenTown));
+		GetNode<Button>("Header/BattleButton").Connect("pressed", this, nameof(OpenBattle));
+		GetNode<Button>("Header/SettingsButton").Connect("pressed", this, nameof(OpenSettings));
+		GetNode<Button>("Header/MenuButton").Connect("pressed", this, nameof(OpenMenu));
 
-        var grid = new GridContainer
-        {
-            Name = "RenderedMap",
-            Columns = adventureMap.Width,
-            RectMinSize = new Vector2(adventureMap.Width * TileSize, adventureMap.Height * TileSize)
-        };
+		mapScroll = GetNode<ScrollContainer>("MapPlaceholder/MapScroll");
+		zoomSlider = GetNode<HSlider>("ZoomPanel/ZoomSlider");
+		zoomLabel = GetNode<Label>("ZoomPanel/ZoomLabel");
+		zoomSlider.Connect("value_changed", this, nameof(SetZoomFromSlider));
+		GetNode<Button>("ZoomPanel/ZoomOutButton").Connect("pressed", this, nameof(ZoomOut));
+		GetNode<Button>("ZoomPanel/ZoomInButton").Connect("pressed", this, nameof(ZoomIn));
 
-        grid.Set("custom_constants/hseparation", 2);
-        grid.Set("custom_constants/vseparation", 2);
-        grid.AnchorLeft = 0.5f;
-        grid.AnchorTop = 0.5f;
-        grid.AnchorRight = 0.5f;
-        grid.AnchorBottom = 0.5f;
-        grid.MarginLeft = -adventureMap.Width * TileSize / 2f;
-        grid.MarginTop = -adventureMap.Height * TileSize / 2f;
-        grid.MarginRight = adventureMap.Width * TileSize / 2f;
-        grid.MarginBottom = adventureMap.Height * TileSize / 2f;
+		adventureMap = GameSession.TakeMapOrDefault();
+		GetNode<Label>("Header/Title").Text = $"Adventure Map - {adventureMap.Name}";
+		RenderAdventureMap();
+		ApplyZoom();
+	}
 
-        mapRoot.AddChild(grid);
+	public override void _Input(InputEvent @event)
+	{
+		if (@event is InputEventMouseButton mouseButton && IsMouseInsideMap(mouseButton.Position))
+		{
+			if (mouseButton.ButtonIndex == (int)ButtonList.WheelUp && mouseButton.Pressed)
+			{
+				ChangeZoom(ZoomStep);
+				GetTree().SetInputAsHandled();
+			}
+			else if (mouseButton.ButtonIndex == (int)ButtonList.WheelDown && mouseButton.Pressed)
+			{
+				ChangeZoom(-ZoomStep);
+				GetTree().SetInputAsHandled();
+			}
+			else if (mouseButton.ButtonIndex == (int)ButtonList.Middle || mouseButton.ButtonIndex == (int)ButtonList.Right)
+			{
+				isPanning = mouseButton.Pressed;
+				lastPanMousePosition = mouseButton.Position;
+				GetTree().SetInputAsHandled();
+			}
+		}
+		else if (@event is InputEventMouseMotion mouseMotion && isPanning)
+		{
+			var delta = mouseMotion.Position - lastPanMousePosition;
+			PanBy(-delta);
+			lastPanMousePosition = mouseMotion.Position;
+			GetTree().SetInputAsHandled();
+		}
+	}
 
-        for (var y = 0; y < adventureMap.Height; y++)
-        {
-            for (var x = 0; x < adventureMap.Width; x++)
-            {
-                grid.AddChild(CreateTileCell(adventureMap.GetTile(x, y)));
-            }
-        }
-    }
+	public override void _Process(float delta)
+	{
+		if (isPanning)
+		{
+			return;
+		}
 
-    private Control CreateTileCell(MapTile tile)
-    {
-        var cell = new ColorRect
-        {
-            RectMinSize = new Vector2(TileSize, TileSize),
-            Color = GetTerrainColor(tile.Terrain)
-        };
+		var mousePosition = GetGlobalMousePosition();
+		if (!IsMouseInsideMap(mousePosition))
+		{
+			return;
+		}
 
-        var label = new Label
-        {
-            AnchorRight = 1,
-            AnchorBottom = 1,
-            Align = Label.AlignEnum.Center,
-            Valign = Label.VAlign.Center,
-            Text = GetMarkerText(tile.Position)
-        };
+		var mapRect = GetNode<Control>("MapPlaceholder").GetGlobalRect();
+		var scrollDelta = Vector2.Zero;
 
-        label.AddColorOverride("font_color", tile.IsPassable ? Colors.White : new Color(0.78f, 0.82f, 0.88f));
-        cell.AddChild(label);
-        return cell;
-    }
+		if (mousePosition.x <= mapRect.Position.x + EdgeScrollSize)
+		{
+			scrollDelta.x -= EdgeScrollSpeed * delta;
+		}
+		else if (mousePosition.x >= mapRect.End.x - EdgeScrollSize)
+		{
+			scrollDelta.x += EdgeScrollSpeed * delta;
+		}
 
-    private string GetMarkerText(MapPosition position)
-    {
-        foreach (var location in adventureMap.Locations)
-        {
-            if (location.Position.X == position.X && location.Position.Y == position.Y)
-            {
-                return GetLocationMarker(location.Type);
-            }
-        }
+		if (mousePosition.y <= mapRect.Position.y + EdgeScrollSize)
+		{
+			scrollDelta.y -= EdgeScrollSpeed * delta;
+		}
+		else if (mousePosition.y >= mapRect.End.y - EdgeScrollSize)
+		{
+			scrollDelta.y += EdgeScrollSpeed * delta;
+		}
 
-        foreach (var unit in adventureMap.Units)
-        {
-            if (unit.Position.X == position.X && unit.Position.Y == position.Y)
-            {
-                return "U";
-            }
-        }
+		if (scrollDelta != Vector2.Zero)
+		{
+			PanBy(scrollDelta);
+		}
+	}
 
-        foreach (var item in adventureMap.Items)
-        {
-            if (item.Position.X == position.X && item.Position.Y == position.Y)
-            {
-                return GetResourceMarker(item.ResourceType);
-            }
-        }
+	private void RenderAdventureMap()
+	{
+		var mapRoot = GetNode<Control>("MapPlaceholder/MapScroll");
+		var chlist = mapRoot.GetChildren();
+		foreach (Node child in added)
+		{
+			child.QueueFree();
+		}
 
-        return string.Empty;
-    }
+		renderedMap = new GridContainer
+		{
+			Name = "RenderedMap",
+			Columns = adventureMap.Width
+		};
+		renderedMap.Set("custom_constants/hseparation", 2);
+		renderedMap.Set("custom_constants/vseparation", 2);
+		mapRoot.AddChild(renderedMap);
+		added.Add(renderedMap);
 
-    private static string GetLocationMarker(SpecialLocationType type)
-    {
-        switch (type)
-        {
-            case SpecialLocationType.Castle:
-                return "C";
-            case SpecialLocationType.Mine:
-                return "M";
-            case SpecialLocationType.Sawmill:
-                return "S";
-            case SpecialLocationType.Shrine:
-                return "+";
-            case SpecialLocationType.Dwelling:
-                return "D";
-            case SpecialLocationType.Treasure:
-                return "T";
-            default:
-                return "?";
-        }
-    }
+		for (var y = 0; y < adventureMap.Height; y++)
+		{
+			for (var x = 0; x < adventureMap.Width; x++)
+			{
+				var node = CreateTileCell(adventureMap.GetTile(x, y));
+				renderedMap.AddChild(node);
+				added.Add(node);
+			}
+		}
+	}
 
-    private static string GetResourceMarker(ResourceType type)
-    {
-        switch (type)
-        {
-            case ResourceType.Gold:
-                return "G";
-            case ResourceType.Wood:
-                return "W";
-            case ResourceType.Ore:
-                return "O";
-            case ResourceType.Crystal:
-                return "C";
-            case ResourceType.Gems:
-                return "J";
-            case ResourceType.Sulfur:
-                return "S";
-            case ResourceType.Mercury:
-                return "M";
-            default:
-                return "?";
-        }
-    }
+	private Control CreateTileCell(MapTile tile)
+	{
+		var cell = new ColorRect
+		{
+			RectMinSize = new Vector2(TileSize, TileSize),
+			Color = GetTerrainColor(tile.Terrain)
+		};
 
-    private static Color GetTerrainColor(TerrainType terrain)
-    {
-        switch (terrain)
-        {
-            case TerrainType.Grass:
-                return new Color(0.18f, 0.42f, 0.18f);
-            case TerrainType.Dirt:
-                return new Color(0.45f, 0.31f, 0.16f);
-            case TerrainType.Road:
-                return new Color(0.58f, 0.49f, 0.31f);
-            case TerrainType.Water:
-                return new Color(0.12f, 0.32f, 0.58f);
-            case TerrainType.Mountain:
-                return new Color(0.30f, 0.32f, 0.36f);
-            case TerrainType.Forest:
-                return new Color(0.08f, 0.25f, 0.12f);
-            default:
-                return Colors.Magenta;
-        }
-    }
+		var label = new Label
+		{
+			AnchorRight = 1,
+			AnchorBottom = 1,
+			Align = Label.AlignEnum.Center,
+			Valign = Label.VAlign.Center,
+			Text = GetMarkerText(tile.Position)
+		};
 
-    private void OpenTown()
-    {
-        GetTree().ChangeScene("res://Scenes/TownScreen.tscn");
-    }
+		label.AddColorOverride("font_color", tile.IsPassable ? Colors.White : new Color(0.78f, 0.82f, 0.88f));
+		cell.AddChild(label);
+		return cell;
+	}
 
-    private void OpenBattle()
-    {
-        GetTree().ChangeScene("res://Scenes/BattleScreen.tscn");
-    }
+	private void SetZoomFromSlider(float value)
+	{
+		zoom = Mathf.Clamp(value / 100.0f, MinZoom, MaxZoom);
+		ApplyZoom();
+	}
 
-    private void OpenSettings()
-    {
-        GetTree().ChangeScene("res://Scenes/Settings.tscn");
-    }
+	private void ZoomOut()
+	{
+		ChangeZoom(-ZoomStep);
+	}
 
-    private void OpenMenu()
-    {
-        GetTree().ChangeScene("res://Scenes/MainMenu.tscn");
-    }
+	private void ZoomIn()
+	{
+		ChangeZoom(ZoomStep);
+	}
+
+	private void ChangeZoom(float delta)
+	{
+		zoom = Mathf.Clamp(zoom + delta, MinZoom, MaxZoom);
+		ApplyZoom();
+	}
+
+	private void ApplyZoom()
+	{
+		var tileSize = Mathf.RoundToInt(TileSize * zoom);
+		var separation = Mathf.Max(1, Mathf.RoundToInt(2 * zoom));
+
+		if (renderedMap != null)
+		{
+			renderedMap.Set("custom_constants/hseparation", separation);
+			renderedMap.Set("custom_constants/vseparation", separation);
+			renderedMap.RectMinSize = new Vector2(adventureMap.Width * (tileSize + separation), adventureMap.Height * (tileSize + separation));
+
+			foreach (Node child in renderedMap.GetChildren())
+			{
+				if (child is Control control)
+				{
+					control.RectMinSize = new Vector2(tileSize, tileSize);
+				}
+			}
+		}
+
+		if (zoomSlider != null && !Mathf.IsEqualApprox((float)zoomSlider.Value, zoom * 100.0f))
+		{
+			zoomSlider.Value = zoom * 100.0f;
+		}
+
+		if (zoomLabel != null)
+		{
+			zoomLabel.Text = $"{Mathf.RoundToInt(zoom * 100.0f)}%";
+		}
+	}
+
+	private void PanBy(Vector2 delta)
+	{
+		mapScroll.ScrollHorizontal = Mathf.Max(0, mapScroll.ScrollHorizontal + Mathf.RoundToInt(delta.x));
+		mapScroll.ScrollVertical = Mathf.Max(0, mapScroll.ScrollVertical + Mathf.RoundToInt(delta.y));
+	}
+
+	private bool IsMouseInsideMap(Vector2 globalPosition)
+	{
+		return GetNode<Control>("MapPlaceholder").GetGlobalRect().HasPoint(globalPosition);
+	}
+
+	private string GetMarkerText(MapPosition position)
+	{
+		foreach (var location in adventureMap.Locations)
+		{
+			if (location.Position.X == position.X && location.Position.Y == position.Y)
+			{
+				return GetLocationMarker(location.Type);
+			}
+		}
+
+		foreach (var unit in adventureMap.Units)
+		{
+			if (unit.Position.X == position.X && unit.Position.Y == position.Y)
+			{
+				return "U";
+			}
+		}
+
+		foreach (var item in adventureMap.Items)
+		{
+			if (item.Position.X == position.X && item.Position.Y == position.Y)
+			{
+				return GetResourceMarker(item.ResourceType);
+			}
+		}
+
+		return string.Empty;
+	}
+
+	private static string GetLocationMarker(SpecialLocationType type)
+	{
+		switch (type)
+		{
+			case SpecialLocationType.Castle:
+				return "C";
+			case SpecialLocationType.Mine:
+				return "M";
+			case SpecialLocationType.Sawmill:
+				return "S";
+			case SpecialLocationType.Shrine:
+				return "+";
+			case SpecialLocationType.Dwelling:
+				return "D";
+			case SpecialLocationType.Treasure:
+				return "T";
+			default:
+				return "?";
+		}
+	}
+
+	private static string GetResourceMarker(ResourceType type)
+	{
+		switch (type)
+		{
+			case ResourceType.Gold:
+				return "G";
+			case ResourceType.Wood:
+				return "W";
+			case ResourceType.Ore:
+				return "O";
+			case ResourceType.Crystal:
+				return "C";
+			case ResourceType.Gems:
+				return "J";
+			case ResourceType.Sulfur:
+				return "S";
+			case ResourceType.Mercury:
+				return "M";
+			default:
+				return "?";
+		}
+	}
+
+	private static Color GetTerrainColor(TerrainType terrain)
+	{
+		switch (terrain)
+		{
+			case TerrainType.Grass:
+				return new Color(0.18f, 0.42f, 0.18f);
+			case TerrainType.Dirt:
+				return new Color(0.45f, 0.31f, 0.16f);
+			case TerrainType.Road:
+				return new Color(0.58f, 0.49f, 0.31f);
+			case TerrainType.Water:
+				return new Color(0.12f, 0.32f, 0.58f);
+			case TerrainType.Mountain:
+				return new Color(0.30f, 0.32f, 0.36f);
+			case TerrainType.Forest:
+				return new Color(0.08f, 0.25f, 0.12f);
+			default:
+				return Colors.Magenta;
+		}
+	}
+
+	private void OpenTown()
+	{
+		GetTree().ChangeScene("res://Scenes/TownScreen.tscn");
+	}
+
+	private void OpenBattle()
+	{
+		GetTree().ChangeScene("res://Scenes/BattleScreen.tscn");
+	}
+
+	private void OpenSettings()
+	{
+		GetTree().ChangeScene("res://Scenes/Settings.tscn");
+	}
+
+	private void OpenMenu()
+	{
+		GetTree().ChangeScene("res://Scenes/MainMenu.tscn");
+	}
 }
