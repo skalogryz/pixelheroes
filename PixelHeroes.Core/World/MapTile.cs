153d0d4e59369a5b9fffd81b81132acccef91429
skalogryz <skalogryz.lists@gmail.com>
2026-05-15 20:06:15 -0400

    adding tiles

--- PixelHeroes.Core/World/MapTile.cs
+++ PixelHeroes.Core/World/MapTile.cs
@@ -9,6 +9,13 @@ namespace PixelHeroes.Core.World
             IsPassable = isPassable;
         }
 
+        // The map creator specific sprite name, to distringuish from the TerrainType.
+        // it's used for the drawing purposes only!
+        //
+        // Note: Sprite isn't necessary a static image, it could be an animation as well
+        // or just a reference to a certain drawer.
+        public string Sprite { get; set; } = "";
+
         public MapPosition Position { get; }
         public TerrainType Terrain { get; set; }
         public bool IsPassable { get; set; }
